f9c9750d74531bc57d87d5d8319ad535bc725864
ssoraka <ssoraka@student.21-school.ru>
2022-09-14 23:42:20 +0300

    init

---
+++ main/java/com/dpisarenko/basiccamundaapp/MinimalCamundaApplication.java
@@ -0,0 +1,11 @@
+package com.dpisarenko.basiccamundaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalCamundaApplication {
+    public static void main(String... args) {
+        SpringApplication.run(MinimalCamundaApplication.class, args);
+    }
+}
